6a6afeb6533397bb3069164a036e39e9854bc97d
Denish Sharma <denishsharma@ternaengg.ac.in>
2022-08-22 21:47:49 +0530

    login page update

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -6,12 +6,12 @@
             <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                 <img class="w-auto h-12 mr-2" src="{{ asset('images/logo-vipdasarathi.png') }}" alt="logo">
             </a>
-            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div class="w-full bg-white rounded-lg overflow-hidden border border-gray-200 sm:max-w-md flex flex-col divide-y">
+                <div class="flex justify-between gap-4 px-6 py-4">
+                    <h4 class="font-semibold text-center w-full">Sign in to your account</h4>
+                </div>
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                        Sign in to your account
-                    </h1>
-                    <form class="space-y-4 md:space-y-6" action="#">
+                    <form class="grid grid-cols-1 gap-4" action="#">
                         <div class="grid grid-cols-1 gap-4 mb-2">
                             <x-input label="Username" placeholder="Enter username" />
                             <x-input label="Password" type="password" placeholder="Enter password" />
@@ -20,11 +20,12 @@
                             <div class="flex items-start">
                                 <x-checkbox id="1" label="Remember Me" />
                             </div>
-                            <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot
-                                                                                                                           password?</a>
+                            <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                         </div>
-                        <x-button primary class="w-full" label="Sign In" />
                     </form>
+                </div>
+                <div class="flex justify-between gap-4 px-6 py-4 bg-gray-50">
+                    <x-button primary class="w-full" label="Sign In" />
                 </div>
             </div>
         </div>
