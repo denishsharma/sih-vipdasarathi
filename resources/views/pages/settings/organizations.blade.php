605b2db722863da1ba5bdd374c8cf428ed44b738
Chandani <chandanimaurya56@gmail.com>
2022-08-24 23:23:58 +0530

    Initial commit

--- resources/views/pages/settings/organizations.blade.php
+++ resources/views/pages/settings/organizations.blade.php
@@ -5,7 +5,7 @@
         <ol class="inline-flex items-center space-x-1 md:space-x-2">
             <li class="inline-flex items-center">
                 <a href="#"
-                    class="inline-flex items-center text-sm font-medium text-gray-400 hover:text-gray-700 dark:text-gray-400 dark:hover:text-white">
+                   class="inline-flex items-center text-sm font-medium text-gray-400 hover:text-gray-700 dark:text-gray-400 dark:hover:text-white">
                     <x-icon class="w-4 h-4 mr-1 mb-0.5" name="home" solid />
                     Home
                 </a>
@@ -14,7 +14,14 @@
                 <div class="flex items-center">
                     <x-icon name="chevron-right" class="w-3 h-3 text-gray-400" />
                     <a href="#"
-                        class="ml-1 text-sm font-medium text-gray-400 hover:text-gray-700 md:ml-2 dark:text-gray-400 dark:hover:text-white">Settings</a>
+                       class="ml-1 text-sm font-medium text-gray-400 hover:text-gray-700 md:ml-2 dark:text-gray-400 dark:hover:text-white">Settings</a>
+                </div>
+            </li>
+            <li>
+                <div class="flex items-center">
+                    <x-icon name="chevron-right" class="w-3 h-3 text-gray-400" />
+                    <span
+                        class="ml-1 text-sm font-medium text-gray-400 hover:text-gray-700 md:ml-2 dark:text-gray-400 dark:hover:text-white">Manage</span>
                 </div>
             </li>
             <li aria-current="page">
@@ -32,5 +39,56 @@
 @endsection
 
 @section('setting-content')
-    <h4 class="text-2xl font-semibold text-gray-700">Manage Organization</h4>
+
+    <div class="overflow-x-auto border border-gray-200 rounded-lg soft-scrollbar">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <caption
+                class="px-5 py-3 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+                Manage Organization
+                <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Some description</p>
+            </caption>
+            <thead class="text-xs font-normal text-gray-500 uppercase bg-stone-100">
+            <tr>
+                <th scope="col" class="py-3 px-6">
+                    Name
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Signature
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Permission
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Users
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Actions
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td class="py-4 px-6">
+                    NDRF
+                </td>
+                <td class="py-4 px-6">
+                    abcd1234
+                </td>
+                <td class="py-4 px-6">
+                    26
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">(View)</a>
+                </td>
+                <td class="py-4 px-6">
+                    22
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">(View)</a>
+                </td>
+                <td class="py-4 px-6 text-right flex items-center justify-start gap-3">
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 @endsection
--- resources/views/pages/settings/sops.blade.php
+++ resources/views/pages/settings/sops.blade.php
@@ -17,6 +17,13 @@
                         class="ml-1 text-sm font-medium text-gray-400 hover:text-gray-700 md:ml-2 dark:text-gray-400 dark:hover:text-white">Settings</a>
                 </div>
             </li>
+            <li>
+                <div class="flex items-center">
+                    <x-icon name="chevron-right" class="w-3 h-3 text-gray-400" />
+                    <span
+                        class="ml-1 text-sm font-medium text-gray-400 hover:text-gray-700 md:ml-2 dark:text-gray-400 dark:hover:text-white">Manage</span>
+                </div>
+            </li>
             <li aria-current="page">
                 <div class="flex items-center">
                     <x-icon name="chevron-right" class="w-3 h-3 text-gray-400" />
@@ -33,5 +40,60 @@
 @endsection
 
 @section('setting-content')
-    <h4 class="text-2xl font-semibold text-gray-700">Manage SOPs</h4>
-@endsection
+        <div class="overflow-x-auto border border-gray-200 rounded-lg soft-scrollbar">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <caption
+                    class="px-5 py-3 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+                    Standard Operating Procedures
+                    <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Some description</p>
+                </caption>
+                <thead class="text-xs font-normal text-gray-500 uppercase bg-stone-100">
+                <tr>
+                    <th scope="col" class="py-3 px-6">
+                        Name
+                    </th>
+                    <th scope="col" class="py-3 px-6">
+                        Signature
+                    </th>
+                    <th scope="col" class="py-3 px-6">
+                        Disaster Type
+                    </th>
+                    <th scope="col" class="py-3 px-6">
+                        File Type
+                    </th>
+                    <th scope="col" class="py-3 px-6">
+                        Created At
+                    </th>
+                    <th scope="col" class="py-3 px-6">
+                        Actions
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td class="py-4 px-6">
+                        SOP Earthquake 2015
+                    </td>
+                    <td class="py-4 px-6">
+                        abcd1234
+                    </td>
+                    <td class="py-4 px-6">
+                        Earthquake
+                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">(View)</a>
+                    </td>
+                    <td class="py-4 px-6">
+                        PDF
+                    </td>
+                    <td class="py-4 px-6">
+                        12 June, 2015
+                    </td>
+                    <td class="py-4 px-6 text-right flex items-center justify-start gap-3">
+                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+
+    @endsection
+
