61d593787a79acb170ed93e5dec5675d9b865aa6
Nagendra Mishra <nagendrasm21@gmail.com>
2022-08-24 23:49:32 +0530

    create case modal form

---
+++ app/Http/Livewire/CreateCaseModal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CreateCaseModal extends Component
+{
+    public function render()
+    {
+        return view('livewire.create-case-modal');
+    }
+}
